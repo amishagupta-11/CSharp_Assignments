62ad54ebe91373f115723ebfe34e40c658fb22cf
Amisha <amishagupta0905@gmail.com>
2023-09-21 16:15:36 +0530

    Three tier application

---
+++ LoginPage/BE/BeLogin.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using DataAccess;
+namespace BusinessLogic
+{
+    /// <summary>
+    /// Specific class for all registration,login and forget password implementations.
+    /// </summary>
+    public class BeLogin
+    {
+        public string _username = "";
+        public string _password = "";
+        public string _Email = "";
+        public string _gender = "";
+
+        /// <summary>
+        /// specific method for registration of a new user.
+        /// </summary>
+        public void Registration()
+          {
+            UserDA obj = new UserDA();     
+            Console.WriteLine("Enter your Email-id: ");
+            _Email = Console.ReadLine();
+            Console.WriteLine("Enter your username: ");
+            _username = Console.ReadLine();  
+            //condition to check if the username is available or not in the database.
+            while (obj.Verify(_username))
+            {
+                Console.WriteLine("Username Not available");
+                Console.WriteLine("Please re-enter your username:");
+                _username=Console.ReadLine();
+            }
+            if(!obj.Verify(_username))
+            {
+
+             Console.WriteLine("Username available.");
+            }            
+            Console.WriteLine("Enter your password: ");
+             _password=Console.ReadLine();
+            Console.WriteLine("Enter your gender: ");
+             _gender = Console.ReadLine();  
+            //method call for checking whether the user has been successfully registered or not.
+            bool isRegistered=obj.RegisterUser(_Email, _username, _password,_gender);   
+            if(isRegistered)
+            {
+                Console.WriteLine(" Registered successfully!!");
+            }
+            else
+            {
+                Console.WriteLine("Registration failed.please try again..");
+            }
+        }
+
+        /// <summary>
+        /// Specific method for user login into the system.
+        /// </summary>
+        public void LoginHere()
+        {
+            UserDA test=new UserDA();
+            Console.WriteLine("Enter your Username ");
+            string _username = Console.ReadLine();
+            Console.WriteLine("Enter your password: ");
+            string _password= Console.ReadLine();
+            //method call to verify the details of user 
+            bool validate = test.LoginUser(_username,_password);
+            if (validate)
+            {
+                Console.WriteLine("Login Successful");
+            }
+            else
+            {
+                //block of code to let user get the option of "Forget password" if they input the incorrect details.
+                string response;
+                Console.WriteLine("Invalid email or password.Please Try again!");
+                Console.WriteLine("Forget Password?");
+                Console.Write("Response: ");
+                 response= (Console.ReadLine());
+                if(response=="Yes"||response=="yes")
+                {
+                    //method call to the reset the password if they choose the forget password option.
+                  ResetPassword();
+                }
+                else
+                {
+                    //recursive call to LoginHere() method if user doesn't want to reset the password.
+                    LoginHere();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Specific method to let user reset their existing password incase they forget the password.
+        /// </summary>
+        public void ResetPassword()
+        {
+            UserDA reset = new UserDA();
+            Console.WriteLine("Please enter your Email-id: ");
+            _Email=Console.ReadLine();
+            Console.WriteLine("Please enter your Username: ");
+            _username=Console.ReadLine();
+            //condition and a method call to verify the user and if verified only then they can change the password.
+            if (reset.validEmail(_Email)&&reset.Verify(_username))
+            {
+                Console.WriteLine("Verified User..");
+                Console.WriteLine("Please enter your new Password: ");
+                _password=Console.ReadLine();
+                //condition to check if the password has been changed or not in the database.
+                if (reset.UpdatePassword(_password,_Email)) 
+                {                
+                Console.WriteLine("Password has Successfully been updated.");
+                }
+            }
+            while (!reset.validEmail(_Email)&&!reset.Verify(_username))
+            {
+                Console.WriteLine("Please enter a valid Email or username!");
+            }
+
+        }
+    }
+}
---
+++ LoginPage/DataAccess/UserDA.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Class for connecting to the SQL database, verifying user details and to store the details in the database.
+    /// </summary>
+    public class UserDA
+    {
+        //creating a new sql connection 
+        public static SqlConnection con = new SqlConnection(@"Data Source=192.168.0.30;Initial Catalog=empid255;Persist Security Info=True;User ID=User5;Password=CDev005#8");
+
+        /// <summary>
+        /// specific method for verifying the username in the database.
+        /// </summary>
+        /// <param name="u_name"></param>
+        /// <returns></returns>
+        public bool Verify(string u_name)
+        {
+            con.Open();
+            string query = "select count(*) from UDetails where Username=@u_name";
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                cmd.Parameters.AddWithValue("@u_name", u_name);
+                int count = (Convert.ToInt32(cmd.ExecuteScalar()));
+                con.Close();
+                return count > 0;
+            }
+        }
+        
+        /// <summary>
+        /// specific method for storing the details of the user into the database.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="gender"></param>
+        /// <returns></returns>
+        public bool RegisterUser(string email,string username,string password,string gender)
+        {
+            con.Open();
+            string query = "insert into UDetails(Email,Username,Password,gender)values(@email,@username,@password,@gender)";
+            using (SqlCommand cmd = new SqlCommand(query, con)) {
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+                cmd.Parameters.AddWithValue("@gender", gender);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                return rowsAffected > 0;
+            }
+
+        }
+        
+        /// <summary>
+        /// specific method to check if the user details are present in the database.
+        /// If details are found the method returns true and login is successful.
+        /// else if login isn't successful.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool LoginUser(string name, string password)
+        {
+            con.Open();
+            String query = "select Username,Password from UDetails where Username=@name and Password=@password";
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                cmd.Parameters.AddWithValue(@"name", name);
+                cmd.Parameters.AddWithValue(@"password", password);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                con.Close();
+                // Check if any rows were returned (i.e., user exists)
+                return dataTable.Rows.Count > 0;
+
+            }
+        }
+        
+        /// <summary>
+        /// Specific method to verify email of an user.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public bool validEmail(string email)
+        {
+            con.Open();
+            string query = "select email from UDetails where Email=@email";
+            using(SqlCommand cmd=new SqlCommand(query, con))
+            {
+                cmd.Parameters.AddWithValue(@"email", email);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                con.Close();
+                // Check if any rows were returned (i.e., user exists)
+                return dataTable.Rows.Count > 0;
+            }
+        }
+
+        /// <summary>
+        /// Specific method that helps the user to change or reset their password.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public bool UpdatePassword(string password,string email)
+        {
+            con.Open();
+            string query = "update UDetails set Password=@password where Email=@email";
+            using(SqlCommand cmd=new SqlCommand(query, con))
+            {
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password",password);
+                int rowsAffect = cmd.ExecuteNonQuery();
+                con.Close();
+                return rowsAffect > 0;
+            }
+        }
+
+    }
+}
