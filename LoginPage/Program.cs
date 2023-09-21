573c5b9ec3155ca826e0b4899f0195b03827559e
Amisha <amishagupta0905@gmail.com>
2023-09-21 15:57:42 +0530

    Three tier Application

---
+++ LoginPage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DataAccess;
+using BE;
+namespace LoginPage 
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+           BeLogin loginObj= new BeLogin();
+            Console.WriteLine("Welcome...");
+            Console.WriteLine("Please Select any one from the below suggestions");
+            Console.WriteLine("1.Login\n2.New User\n3.Forget Password\n 4.Exit");
+            int select=Int32.Parse(Console.ReadLine());
+            while(select>0 && select<5)
+            {
+                if (select==1)
+                {
+                    loginObj.LoginHere();
+                }
+                if (select==2)
+                {
+                    loginObj.Registration();
+                }
+            }
+        }
+    }
+}
