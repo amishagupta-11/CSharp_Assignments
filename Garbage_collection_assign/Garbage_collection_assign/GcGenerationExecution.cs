4beb4712d4dae86ea1a9956012630a60bddde996
Amisha <amishagupta0905@gmail.com>
2023-09-21 17:33:50 +0530

    Garbagecollection

---
+++ Garbage_collection_assign/Garbage_collection_assign/GcGenerationExecution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace GCSystem
+{
+    /// <summary>
+    /// Adding class to check execution time taken by objects to move between generations 
+    /// </summary>
+   public class GcGenerationExecution
+    {        
+        /// <summary>
+        /// method to find the time taken by objects to move between generations
+        /// </summary>
+        /// <param name="obj"></param>
+        public void ExecutionTime(object obj)
+        {
+            int count = 0;
+            int countV = 0;
+            //using DateTime function to start the timer when the objects are in gen 0 
+            //finding the generation of an obj using garbageCOllection method i.e..,GC.GetGeneration()
+            DateTime start = DateTime.Now;
+            while (GC.GetGeneration(obj)==0 )
+            {
+                //creation of multiple objects and then incrementing the counter after every iteration 
+                GcGenerationExecution firstObj = new GcGenerationExecution();
+                GcGenerationExecution secondObj= new GcGenerationExecution();
+                GcGenerationExecution thirdObj = new GcGenerationExecution();
+                GcGenerationExecution fourthObj = new GcGenerationExecution();
+                count++;
+            }
+            //stopping the timer after all the objects have moved from gen0 to gen 1
+            DateTime end = DateTime.Now;
+            //calculating time taken 
+            TimeSpan ts = end-start;           
+            Console.WriteLine("Execution time in ms:{0}", ts.TotalMilliseconds+ " ms ");
+            Console.WriteLine("Objects executed :"+count);            
+            //starting the timer to check time taken by obj to move from gen 0 to gen 1
+            DateTime startIt = DateTime.Now;
+            while (GC.GetGeneration(obj)==1)
+            {
+                //creation of multiple objects while the first object is in first generation
+                GcGenerationExecution firstObj = new GcGenerationExecution();
+                GcGenerationExecution secondObj = new GcGenerationExecution();
+                GcGenerationExecution thirdObj = new GcGenerationExecution();
+                GcGenerationExecution fourthObj = new GcGenerationExecution();
+                //incrementing counter variable
+                countV++;
+            }
+            Console.WriteLine("Address after first Generation:"+obj.GetHashCode());
+            //stopping the timer 
+            DateTime endIT = DateTime.Now;
+            //calculating the time span 
+            TimeSpan tsIt = endIT-startIt;
+            //displaying the execution time ,total iterations and current generation when objects have moved from gen 1 to gen 2
+            Console.WriteLine("Execution time in ms :{0}", tsIt.TotalMilliseconds+ " ms ");
+            Console.WriteLine("Objects Executed: "+countV);
+            Console.WriteLine("Generation after second loop:"+ GC.GetGeneration(obj));
+        }
+      
+    }
+}
+
---
+++ Garbage_collection_assign/Garbage_collection_assign/Program.cs
@@ -0,0 +1,81 @@
+﻿using GCSystem;
+using System;
+
+
+namespace GcSystemtime
+{
+    public class Program
+    {
+       /* public static void Main()
+        {
+            //creation object of GcGenerationExecution class
+           GcGenerationExecution obj = new GcGenerationExecution();
+            ObjectPart obj1=new ObjectPart();
+
+            //calling ExecutionTime method by passing obj as parameter
+            /* Console.WriteLine(obj.GetHashCode());
+             obj.ExecutionTime(obj);*/
+           
+            /*Practice pt=new Practice(10,12);
+            Console.WriteLine("First object address:"+pt.GetHashCode());
+            Practice pt1= new Practice(14,05);   
+            Console.WriteLine("Second object address:"+pt1.GetHashCode());
+            Happy hp = new Happy();
+            hp.Meth();
+            Console.WriteLine("Third object address:"+hp.GetHashCode());
+            obj1.generationPart(pt1);
+            
+        }*/
+            private const long maxGarbage = 1000;
+
+            static void Main()
+            {
+                Program myGCCol = new Program();
+                Console.WriteLine(myGCCol.GetHashCode());
+
+                // Determine the maximum number of generations the system
+                // garbage collector currently supports.
+                Console.WriteLine("The highest generation is {0}", GC.MaxGeneration);
+
+                myGCCol.MakeSomeGarbage();
+
+                // Determine which generation myGCCol object is stored in.
+                Console.WriteLine("Generation: {0}", GC.GetGeneration(myGCCol));
+
+                // Determine the best available approximation of the number
+                // of bytes currently allocated in managed memory.
+                Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+
+                // Perform a collection of generation 0 only.
+                GC.Collect(0);
+
+                // Determine which generation myGCCol object is stored in.
+                Console.WriteLine("Generation: {0}", GC.GetGeneration(myGCCol));
+                
+
+                Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+
+                // Perform a collection of all generations up to and including 2.
+                GC.Collect(2);
+
+                // Determine which generation myGCCol object is stored in.
+                Console.WriteLine("Generation: {0}", GC.GetGeneration(myGCCol));
+                Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+                Console.Read();
+            }
+
+            void MakeSomeGarbage()
+            {
+                Version vt;
+
+                for (int i = 0; i < maxGarbage; i++)
+                {
+                    // Create objects and release them to fill up memory
+                    // with unused objects.
+                    vt = new Version();
+                }
+            }
+        }
+    }
+
+
