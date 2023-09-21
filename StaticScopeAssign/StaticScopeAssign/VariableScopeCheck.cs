c837d5aacbabd5c9f29a43d24bf0c79d8b44a170
Amisha <amishagupta0905@gmail.com>
2023-09-21 17:20:42 +0530

    Scope Assignment

---
+++ StaticScopeAssign/StaticScopeAssign/VariableScopeCheck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ScopeTesting 
+{
+    /// <summary>
+    /// Creating class for defining the different static and non-static variables
+    /// </summary>
+    public class VariableScopeCheck
+    {
+        //declaring and defining static variables
+        public static int _resultStorage = 0;
+        public static string Type = "Arithmetic";
+        /// <summary>
+        /// creating method to add 2 values
+        /// </summary>
+        /// <param name="fNum"></param>
+        /// <param name="sNum"></param>
+        /// <returns></returns>
+        public static int Sum(int fNum, int sNum)
+        {
+            return fNum + sNum;
+        }
+        /// <summary>
+        /// creating a method to return the value
+        /// </summary>
+        /// <param name="result"></param>
+        public static void Store(int result)
+        {
+            _resultStorage = result;
+        }
+    }
+    /// <summary>
+    /// Creating a separate class for re-initializing the values and checking their scope
+    /// </summary>
+    class Validate
+    {
+        static void Main(string[] args)
+        {
+            // calling static method
+            int result = VariableScopeCheck.Sum(10, 25);
+            //displaying the value
+            Console.WriteLine(result);
+            // accessing static variable
+            string calcType = VariableScopeCheck.Type;
+            // assign value to static variable
+            VariableScopeCheck.Type = "Scientific"; 
+            Console.WriteLine(VariableScopeCheck.Type);
+        }
+    }
+}
---
+++ StaticScopeAssign/testing/ScopeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ScopeTesting;
+/// <summary>
+/// creating another class in same solution but different project to check scope of static variables
+/// </summary>
+public class ScopeTest
+{
+    //declaring and initializing global variable
+    int n = 100;  
+    public static void Main()
+    {
+        //modifying the static values defined in VariableScopeCheck class 
+        VariableScopeCheck.Type="SCIENCE";
+        int result = VariableScopeCheck.Sum(24, 25);
+        //displaying the results obtained
+        Console.WriteLine(result);
+        Console.WriteLine(VariableScopeCheck.Type);
+    }
+}
